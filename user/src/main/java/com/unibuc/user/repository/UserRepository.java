4449bc2f12781768bc5071c268b312d07c15116d
marinescuclaudiu <claudiumarinescu@yahoo.com>
2024-06-20 21:43:30 +0300

    Remove user project

--- user/src/main/java/com/unibuc/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unibuc.user.repository;
-
-import com.unibuc.user.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
--- user/src/main/java/com/unibuc/user/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.unibuc.user.service;
-
-import com.unibuc.user.model.User;
-
-public interface IUserService {
-    User save(User user);
-}
